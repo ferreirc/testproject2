4383da9fa2edebb485d2d2d90acc6e4a86d3d80b
Christopher Ferreira <chris@cdfdev.com>
2017-05-12 09:23:08 -0400

    Initial Commit

---
+++ testproject2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testproject2
+//
+//  Created by Christopher Ferreira on 5/12/17.
+//  Copyright © 2017 Christopher Ferreira Consulting Services. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
